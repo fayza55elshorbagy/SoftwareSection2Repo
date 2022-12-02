b0682d43f90efafb4b7375eb309ad6919690efc6
OmarKhaled1123 <oma1123512@gmail.com>
2022-12-02 14:22:16 +0200

    Added new c++ file

---
+++ my_first_class.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+using namespace std;
+class triangle {
+private:
+	float base;
+	float height;
+public:
+	triangle() {
+		cout << "P constructor\n";
+		base = 0;
+		height = 0;
+	}
+	triangle (float a, float b) {
+		 a = base;
+		 b = height;
+	}
+	void setBase_Height(float b, float h) {
+		base = b;
+		height = h;
+
+	}
+	float area() {
+		return 0.5 * base * height;
+	}
+	void print() {
+		cout << "base=" << base << endl << "height=" << height << endl << "area=" << area() << endl;
+	}
+		
+};
+int main() {
+
+	triangle ob;
+	//ob.setBase_Height(5.5, 10.6);
+	ob.print();
+	
+}
